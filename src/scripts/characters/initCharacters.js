8efb9722b9a2b0935c86d3608557c1ac710f99e4
pitzbro <asioren@gmail.com>
2022-09-08 17:40:27 +0300

    working on player

--- src/scripts/characters/initCharacters.js
+++ src/scripts/characters/initCharacters.js
@@ -11,3 +11,42 @@ export default function initPlayer() {
 export function initCharacter() {
     
 }
+
+document.onkeydown = checkKeyDown;
+document.onkeyup = checkKeyUp;
+
+function checkKeyDown(e) {
+
+    e = e || window.event;
+
+    switch (e.keyCode) {
+        case 38:
+            // up arrow
+            player.dataset.direction = 'up';
+            player.dataset.walk = 'true';
+            break;
+        case 40:
+            // down arrow
+            player.dataset.direction = 'down';
+            player.dataset.walk = 'true';
+            break;
+        case 37:
+            // left arrow
+            player.dataset.direction = 'left';
+            player.dataset.walk = 'true';
+            break;
+        case 39:
+            // down arrow
+            player.dataset.direction = 'right';
+            player.dataset.walk = 'true';
+            break;
+    
+        default:
+            break;
+    }
+
+}
+
+function checkKeyUp(e) {
+    player.dataset.walk = 'false';
+}
